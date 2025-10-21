77ff5d8a0ad141e34edc849014dcbcc7e7a968af
Sebastian Fischer <sebf.fischer@gmail.com>
2025-10-21 13:40:18 +0200

    lintr, spelling

--- tests/testthat/test-primitives-meta.R
+++ tests/testthat/test-primitives-meta.R
@@ -5,10 +5,13 @@ test_that("Jit rule is tested", {
   tests_dir <- testthat::test_path()
   candidate_files <- c(
     system.file("extra-tests", "test-primitives-jit-torch.R", package = "anvil"),
-    system.file("tests", "testthat", "test-primitives-jit.R", package = "anvil")
+    file.path(testthat::test_path(), "test-primitives-jit.R")
   )
 
-  content <- paste(vapply(candidate_files, function(file) paste(readLines(file, warn = FALSE), collapse = "\n"), character(1L)), collapse = "\n")
+  content <- paste(
+    vapply(candidate_files, function(file) paste(readLines(file, warn = FALSE), collapse = "\n"), character(1L)),
+    collapse = "\n"
+  )
   missing <- Filter(function(nm) !grepl(paste0('test_that("', nm), content, fixed = TRUE), primitive_names)
 
   expect_true(length(missing) == 0L, info = paste(missing, collapse = ", "), label = "Jit rule is tested")
@@ -26,13 +29,15 @@ test_that("Pullback rule is tested", {
     primitive_names
   )
 
-  tests_dir <- testthat::test_path()
   candidate_files <- c(
     system.file("extra-tests", "test-primitives-pullback-torch.R", package = "anvil"),
-    system.file("tests", "testthat", "test-primitives-pullback.R", package = "anvil")
+    file.path(testthat::test_path(), "test-primitives-pullback.R")
   )
 
-  content <- paste(vapply(candidate_files, function(file) paste(readLines(file, warn = FALSE), collapse = "\n"), character(1L)), collapse = "\n")
+  content <- paste(
+    vapply(candidate_files, function(file) paste(readLines(file, warn = FALSE), collapse = "\n"), character(1L)),
+    collapse = "\n"
+  )
   missing <- Filter(function(nm) !grepl(paste0('test_that("', nm), content, fixed = TRUE), primitive_names)
   expect_true(length(missing) == 0L, info = paste(missing, collapse = ", "), label = "Pullback rule is tested")
 })
