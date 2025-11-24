33eea765d15f51fb601ef5c2138ecfb6ac920f75
Sebastian Fischer <sebf.fischer@gmail.com>
2025-11-24 10:38:58 +0100

    fix runif

--- R/api.R
+++ R/api.R
@@ -508,10 +508,7 @@ nv_runif <- function(initial_state, dtype = "f64", shape_out, lower = 0, upper =
   )
 
   lhs <- nv_convert(rbits[[2]], dtype = dtype)
-  rhs <- nv_convert(
-    nv_maxval(paste0("ui", sub("f(\\d+)", "\\1", dtype)), device = NULL),
-    dtype = dtype
-  )
+  rhs <- nv_maxval(dtype, device(initial_state))
   U <- nv_div(lhs, rhs)
   if (range != 1) {
     U <- nv_mul(U, nv_scalar(range, dtype = dtype))
