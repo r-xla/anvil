6040924b22d1518cdef65adce3802fb386087bf9
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-12-14 11:20:51 -0500

    mnist: strengthen internal helper tests

--- tests/testthat/test-mnist-utils.R
+++ tests/testthat/test-mnist-utils.R
@@ -39,3 +39,29 @@ test_that("internal MNIST helpers: invalid rds returns NULL", {
   expect_null(anvil:::.load_mnist(rds_path = tmp, pkgs = character()))
 })
 
+test_that("internal MNIST helpers: one_hot does not shift 1-based labels", {
+  y <- as.integer(1:4)
+  y_one_hot <- anvil:::.mnist_one_hot(y, nclass = 10L)
+  expect_equal(dim(y_one_hot), c(4L, 10L))
+  expect_equal(rowSums(y_one_hot), rep(1, 4L))
+  expect_equal(max.col(y_one_hot, ties.method = "first"), y)
+})
+
+test_that("internal MNIST helpers: preprocess respects train_n/test_n", {
+  mnist <- list(
+    train = list(
+      x = array(255, dim = c(5L, 28L, 28L)),
+      y = as.integer(0:4)
+    ),
+    test = list(
+      x = array(0, dim = c(4L, 28L, 28L)),
+      y = as.integer(0:3)
+    )
+  )
+
+  data <- anvil:::.prepare_mnist_mlp_data(mnist, train_n = 100L, test_n = 2L)
+  expect_equal(dim(data$train$x), c(5L, 784L))
+  expect_equal(dim(data$test$x), c(2L, 784L))
+  expect_equal(as.numeric(range(data$train$x)), c(1, 1))
+  expect_equal(as.numeric(range(data$test$x)), c(0, 0))
+})
