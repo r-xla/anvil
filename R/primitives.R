d47c0eee4d49556002b3c82d8a163b4d9da950e9
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-12-15 18:54:39 -0500

    update stablehlo

--- R/primitives.R
+++ R/primitives.R
@@ -3,7 +3,7 @@
 
 # Type inference helper functions for graph building
 #' @importFrom stablehlo infer_types_generic_biv infer_types_generic_uni
-#' @importFrom stablehlo infer_types_integerish_biv infer_types_boolean_uni
+#' @importFrom stablehlo infer_types_integerish_biv
 #' @importFrom stablehlo infer_types_compare infer_types_transpose infer_types_reshape
 #' @importFrom stablehlo infer_types_broadcast_in_dim infer_types_convert
 #' @importFrom stablehlo infer_types_dot_general infer_types_select
@@ -20,9 +20,7 @@ infer_binary_integerish <- function(lhs, rhs) {
   stablehlo::infer_types_integerish_biv(lhs, rhs)@items
 }
 
-infer_unary_integerish <- function(operand) {
-  stablehlo::infer_types_boolean_uni(operand)@items
-}
+
 
 infer_reduce <- function(operand, dims, drop) {
   old_shape <- operand@type@shape@dims
