72e3c32e75a7dc9762675af8b5ff450e1b0b8fb3
Sebastian Fischer <sebf.fischer@gmail.com>
2026-04-14 11:10:26 +0000

    ...

--- man-roxygen/param_backend.R
+++ man-roxygen/param_backend.R
@@ -1,4 +1,5 @@
 #' @param backend (`character(1)`)\cr
-#'   Backend used to construct the array. `"auto"` (default) picks a backend
+#'   Backend used to construct the array. `"auto"` picks a backend
 #'   based on the inputs (or [`default_backend()`] when there are none).
-#'   See [`jit()`] for the list of available backends.
+#'   Other backends are `"xla"` and `"quickr"` (experimental).
+#'   
