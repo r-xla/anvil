6aeb31931a59de5dd8b93ff541ffcd328e8b90e1
Sebastian Fischer <sebf.fischer@gmail.com>
2026-05-18 12:53:11 +0000

    reference cuda license

--- R/aaa.R
+++ R/aaa.R
@@ -1,5 +1,16 @@
 #' @keywords internal
 NULL
+
+#' @section Third-Party Licenses:
+#' The `anvl` package itself is MIT-licensed. The CUDA backend dynamically
+#' loads NVIDIA software which is not bundled with `anvl`, but downloaded
+#' from NVIDIA's official redistributable channels by the CUDA toolkit R
+#' package (e.g. `cuda12.8`) at install time. Its use is governed by the
+#' [NVIDIA CUDA Toolkit EULA](https://docs.nvidia.com/cuda/eula/), with the
+#' exception of cuDNN, which is covered by the
+#' [NVIDIA cuDNN SLA](https://docs.nvidia.com/deeplearning/cudnn/sla/index.html),
+#' and NCCL, which is covered by its [own license](https://github.com/NVIDIA/nccl/blob/master/LICENSE.txt).
+#' By installing or using the CUDA backend you accept those terms.
 "_PACKAGE"
 
 ## usethis namespace: start
